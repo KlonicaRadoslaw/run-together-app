2246f297c593c31840ee93df980fa409623b2700
Kamilek <radek71k@onet.pl>
2024-08-09 10:52:46 +0200

    Club and Race index page

--- RunTogetherWebApp/Controllers/ClubController.cs
+++ RunTogetherWebApp/Controllers/ClubController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunTogetherWebApp.Data;
 
 namespace RunTogetherWebApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public ClubController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
--- RunTogetherWebApp/Controllers/RaceController.cs
+++ RunTogetherWebApp/Controllers/RaceController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunTogetherWebApp.Data;
 
 namespace RunTogetherWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context) 
+        { 
+            _context = context; 
+        }
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
