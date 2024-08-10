596e3032afde6bf4ab1f0c5cff7c4a575a2de22e
Kamilek <radek71k@onet.pl>
2024-08-10 14:23:50 +0200

    Image upload while creating race

--- RunTogetherWebApp/Controllers/RaceController.cs
+++ RunTogetherWebApp/Controllers/RaceController.cs
@@ -4,16 +4,20 @@ using RunTogetherWebApp.Data;
 using RunTogetherWebApp.Interfaces;
 using RunTogetherWebApp.Models;
 using RunTogetherWebApp.Repositories;
+using RunTogetherWebApp.Services;
+using RunTogetherWebApp.ViewModels;
 
 namespace RunTogetherWebApp.Controllers
 {
     public class RaceController : Controller
     {
         private readonly IRaceRepository _raceRepository;
+        private readonly IPhotoService _photoService;
 
-        public RaceController(IRaceRepository raceRepository)
+        public RaceController(IRaceRepository raceRepository, IPhotoService photoService)
         {
             _raceRepository = raceRepository;
+            _photoService = photoService;
         }
         public async Task<IActionResult> Index()
         {
@@ -33,14 +37,31 @@ namespace RunTogetherWebApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Race race)
+        public async Task<IActionResult> Create(CreateRaceViewModel raceVM)
         {
-            if (!ModelState.IsValid)
-                return View(race);
+            if (ModelState.IsValid)
+            {
+                var result = await _photoService.AddPhotoAsync(raceVM.Image);
 
-            _raceRepository.Add(race);
+                var race = new Race
+                {
+                    Title = raceVM.Title,
+                    Description = raceVM.Description,
+                    Image = result.Url.ToString(),
+                    Address = new Address
+                    {
+                        Street = raceVM.Address.Street,
+                        City = raceVM.Address.City,
+                        State = raceVM.Address.State,
+                    }
+                };
+                _raceRepository.Add(race);
+                return RedirectToAction("Index");
+            }
+            else
+                ModelState.AddModelError("", "Photo upload failed");
 
-            return RedirectToAction("Index");
+            return View();
         }
     }
 }
