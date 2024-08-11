42aa5e03df8600bb2d54fdd77461169c4f71590b
Kamilek <radek71k@onet.pl>
2024-08-11 11:52:03 +0200

    Identity setup and seed

--- RunTogetherWebApp/Program.cs
+++ RunTogetherWebApp/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RunTogetherWebApp.Data;
 using RunTogetherWebApp.Helpers;
 using RunTogetherWebApp.Interfaces;
+using RunTogetherWebApp.Models;
 using RunTogetherWebApp.Repositories;
 using RunTogetherWebApp.Services;
 
@@ -19,13 +22,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
 
 var app = builder.Build();
 
 if (args.Length == 1 && args[0].ToLower() == "seeddata")
 {
-    //Seed.SeedUsersAndRolesAsync(app);
-    Seed.SeedData(app);
+    Seed.SeedUsersAndRolesAsync(app);
+    //Seed.SeedData(app);
 }
 
 // Configure the HTTP request pipeline.
