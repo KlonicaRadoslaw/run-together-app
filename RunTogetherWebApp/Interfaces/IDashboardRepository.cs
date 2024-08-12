086096368a549d618a3c5f7c083606cd0050dd9b
Kamilek <radek71k@onet.pl>
2024-08-12 16:54:08 +0200

    Dashboard repository

---
+++ RunTogetherWebApp/Interfaces/IDashboardRepository.cs
@@ -0,0 +1,10 @@
+﻿using RunTogetherWebApp.Models;
+
+namespace RunTogetherWebApp.Interfaces
+{
+    public interface IDashboardRepository
+    {
+        Task<List<Race>> GetAllUserRaces();
+        Task<List<Club>> GetAllUserClubs();
+    }
+}
---
+++ RunTogetherWebApp/Repositories/DashboardRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunTogetherWebApp.Data;
+using RunTogetherWebApp.Interfaces;
+using RunTogetherWebApp.Models;
+
+namespace RunTogetherWebApp.Repositories
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public DashboardRepository(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<List<Club>> GetAllUserClubs()
+        {
+            var currentUser = _httpContextAccessor.HttpContext?.User;
+            var userClubs = await _context.Clubs
+                                    .Where(r => r.AppUser.Id == currentUser.ToString()).ToListAsync();
+
+            return userClubs;
+        }
+
+        public async Task<List<Race>> GetAllUserRaces()
+        {
+            var currentUser = _httpContextAccessor.HttpContext?.User;
+            var userRaces = await _context.Races
+                                    .Where(r => r.AppUser.Id == currentUser.ToString()).ToListAsync();
+
+            return userRaces;
+        }
+    }
+}
