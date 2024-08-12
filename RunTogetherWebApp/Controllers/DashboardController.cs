91470af4e7085891eece761dc2ba4ca3240d681a
Kamilek <radek71k@onet.pl>
2024-08-12 17:15:14 +0200

    Dashboard Index

--- RunTogetherWebApp/Controllers/DashboardController.cs
+++ RunTogetherWebApp/Controllers/DashboardController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunTogetherWebApp.Interfaces;
+using RunTogetherWebApp.ViewModels;
 
 namespace RunTogetherWebApp.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly IDashboardRepository _dashboardRepository;
+
+        public DashboardController(IDashboardRepository dashboardRepository)
         {
-            return View();
+            _dashboardRepository = dashboardRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userRaces = await _dashboardRepository.GetAllUserRaces();
+            var userClubs = await _dashboardRepository.GetAllUserClubs();
+            var dashboardViewModel = new DashboardViewModel()
+            {
+                Races = userRaces,
+                Clubs = userClubs
+            };
+            return View(dashboardViewModel);
         }
     }
 }
---
+++ RunTogetherWebApp/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,10 @@
+﻿using RunTogetherWebApp.Models;
+
+namespace RunTogetherWebApp.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public List<Race> Races { get; set; }
+        public List<Club> Clubs { get; set; }
+    }
+}
