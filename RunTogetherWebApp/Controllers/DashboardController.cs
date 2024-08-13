7094b1bab53a0036f6d9335ad9f083ee4b8fb2b6
Kamilek <radek71k@onet.pl>
2024-08-13 14:18:55 +0200

    Edit user profile
    Prep for editing user's profile.
    DashBoard repository update.
    AppUser changes.

--- RunTogetherWebApp/Controllers/DashboardController.cs
+++ RunTogetherWebApp/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunTogetherWebApp.Extensions;
 using RunTogetherWebApp.Interfaces;
 using RunTogetherWebApp.ViewModels;
 
@@ -7,10 +8,16 @@ namespace RunTogetherWebApp.Controllers
     public class DashboardController : Controller
     {
         private readonly IDashboardRepository _dashboardRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IPhotoService _photoService;
 
-        public DashboardController(IDashboardRepository dashboardRepository)
+        public DashboardController(IDashboardRepository dashboardRepository, 
+                                   IHttpContextAccessor httpContextAccessor,
+                                   IPhotoService photoService)
         {
             _dashboardRepository = dashboardRepository;
+            _httpContextAccessor = httpContextAccessor;
+            _photoService = photoService;
         }
 
         public async Task<IActionResult> Index()
@@ -23,6 +30,27 @@ namespace RunTogetherWebApp.Controllers
                 Clubs = userClubs
             };
             return View(dashboardViewModel);
+        }
+
+        public async Task<IActionResult> EditUserProfile()
+        {
+            var currentUserId = _httpContextAccessor.HttpContext?.User.GetUserId();
+            var user = await _dashboardRepository.GetUserById(currentUserId);
+
+            if (user == null)
+                return View("Error");
+
+            var editUserViewModel = new EditUserDashboardViewModel()
+            {
+                Id = currentUserId,
+                Pace = user.Pace,
+                Mileage = user.Milage,
+                ProfileImageUrl = user.ProfileImageUrl,
+                City = user.City,
+                State = user.State
+            };
+
+            return View();
         }
     }
 }
--- RunTogetherWebApp/Repositories/DashboardRepository.cs
+++ RunTogetherWebApp/Repositories/DashboardRepository.cs
@@ -35,5 +35,30 @@ namespace RunTogetherWebApp.Repositories
 
             return userRaces;
         }
+
+        public async Task<AppUser> GetByIdNoTracking(string id)
+        {
+            return await _context.Users
+                                 .Where(u => u.Id == id)
+                                 .AsNoTracking()
+                                 .FirstOrDefaultAsync();
+        }
+
+        public async Task<AppUser> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+
+        public bool Update(AppUser user)
+        {
+            _context.Users.Update(user);
+            return Save();
+        }
     }
 }
