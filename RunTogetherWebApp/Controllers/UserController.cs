7880837b5f25c88588a7768eeb841ae087b47f4a
Kamilek <radek71k@onet.pl>
2024-08-13 13:41:52 +0200

    Runners display

--- RunTogetherWebApp/Controllers/UserController.cs
+++ RunTogetherWebApp/Controllers/UserController.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunTogetherWebApp.Interfaces;
+using RunTogetherWebApp.ViewModels;
 
 namespace RunTogetherWebApp.Controllers
 {
     public class UserController : Controller
     {
-        [HttpGet("users")]
-        public IActionResult Index()
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
         {
-            return View();
+            _userRepository = userRepository;
+        }
+
+
+        [HttpGet("users")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsers();
+            var result = new List<UserViewModel>();
+            foreach (var user in users)
+            {
+                var userViewModel = new UserViewModel()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Pace = user.Pace,
+                    Mileage = user.Milage
+                };
+                result.Add(userViewModel);
+            }
+            return View(result);
         }
     }
 }
