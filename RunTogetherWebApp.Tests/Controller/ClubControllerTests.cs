160c28c45941bc653dda795c7d3733522218458d
Kamilek <radek71k@onet.pl>
2024-08-18 12:25:49 +0200

    Club Controller and Repository tests

--- RunTogetherWebApp.Tests/Controller/ClubControllerTests.cs
+++ RunTogetherWebApp.Tests/Controller/ClubControllerTests.cs
@@ -5,11 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 using RunTogetherWebApp.Controllers;
 using RunTogetherWebApp.Interfaces;
 using RunTogetherWebApp.Models;
+using RunTogetherWebApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 
 namespace RunTogetherWebApp.Tests.Controller
 {
@@ -18,43 +21,124 @@ namespace RunTogetherWebApp.Tests.Controller
         private ClubController _clubController;
         private IClubRepository _clubRepository;
         private IPhotoService _photoService;
-        private IHttpContextAccessor _httpContextAccessor;
         public ClubControllerTests()
         {
             //Dependencies
             _clubRepository = A.Fake<IClubRepository>();
             _photoService = A.Fake<IPhotoService>();
-            _httpContextAccessor = A.Fake<HttpContextAccessor>();
 
             //SUT
-            _clubController = new ClubController(_clubRepository, _photoService, _httpContextAccessor);
+            _clubController = new ClubController(_clubRepository, _photoService);
         }
 
         [Fact]
-        public void ClubController_Index_ReturnsSuccess()
+        public async Task Index_ReturnsViewResult_WithClubList()
         {
-            //Arrange
-            var clubs = A.Fake<IEnumerable<Club>>();
-            A.CallTo(() => _clubRepository.GetAll()).Returns(clubs);
+            // Arrange
+            var clubs = new List<Club> { new Club(), new Club() };
+            A.CallTo(() => _clubRepository.GetSliceAsync(A<int>.Ignored, A<int>.Ignored)).Returns(clubs);
+            A.CallTo(() => _clubRepository.GetCountAsync()).Returns(2);
 
-            //Act
-            var result = _clubController.Index();
+            // Act
+            var result = await _clubController.Index();
 
-            //Assert
-            result.Should().BeOfType<Task<IActionResult>>();
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IndexClubViewModel>(viewResult.Model);
+            int count = model.Clubs.Count();
+            Assert.Equal(2, count);
         }
 
         [Fact]
-        public void ClubController_Detail_ReturnsSuccess()
+        public async Task Detail_ValidId_ReturnsViewResult_WithClub()
         {
-            //Arrange
-            var id = 1;
-            var club = A.Fake<Club>();
-            A.CallTo(() => _clubRepository.GetById(id)).Returns(club);
-            //Act
-            var result = _clubController.Detail(id);
-            //Assert
-            result.Should().BeOfType<Task<IActionResult>>();
+            // Arrange
+            var club = new Club { Id = 1, Title = "Test Club" };
+            A.CallTo(() => _clubRepository.GetById(1)).Returns(Task.FromResult(club));
+
+            // Act
+            var result = await _clubController.Detail(1);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<Club>(viewResult.Model);
+            Assert.Equal(club, model);
+        }
+
+        [Fact]
+        public async Task Detail_InvalidId_ReturnsNotFoundResult()
+        {
+            // Arrange
+            A.CallTo(() => _clubRepository.GetById(A<int>.Ignored)).Returns(Task.FromResult<Club>(null));
+
+            // Act
+            var result = await _clubController.Detail(1);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        /*[Fact]
+        public async Task Create_ValidModel_RedirectsToIndex()
+        {
+            // Arrange
+            var viewModel = new CreateClubViewModel { Title = "New Club", Image = null };
+            var photoUploadResult = new ImageUploadResult { Url = new Uri("http://test.com/photo.jpg") };
+            A.CallTo(() => _photoService.AddPhotoAsync(viewModel.Image)).Returns(Task.FromResult(photoUploadResult));
+
+            // Act
+            var result = await _clubController.Create(viewModel);
+
+            // Assert
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
+        }*/
+
+        [Fact]
+        public async Task Edit_ValidId_ReturnsViewResult_WithEditClubViewModel()
+        {
+            // Arrange
+            var club = new Club { Id = 1, Title = "Test Club", Address = new Address() };
+            A.CallTo(() => _clubRepository.GetById(1)).Returns(Task.FromResult(club));
+
+            // Act
+            var result = await _clubController.Edit(1);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<EditClubViewModel>(viewResult.Model);
+            Assert.Equal("Test Club", model.Title);
+        }
+
+        [Fact]
+        public async Task Delete_ValidId_ReturnsViewResult_WithClub()
+        {
+            // Arrange
+            var club = new Club { Id = 1, Title = "Test Club" };
+            A.CallTo(() => _clubRepository.GetById(1)).Returns(Task.FromResult(club));
+
+            // Act
+            var result = await _clubController.Delete(1);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<Club>(viewResult.Model);
+            Assert.Equal(club, model);
+        }
+
+        [Fact]
+        public async Task DeleteClub_ValidId_RedirectsToIndex()
+        {
+            // Arrange
+            var club = new Club { Id = 1, Title = "Test Club" };
+            A.CallTo(() => _clubRepository.GetById(1)).Returns(Task.FromResult(club));
+
+            // Act
+            var result = await _clubController.DeleteClub(1);
+
+            // Assert
+            var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirectResult.ActionName);
         }
     }
 }
--- RunTogetherWebApp.Tests/Repository/ClubRepositoryTests.cs
+++ RunTogetherWebApp.Tests/Repository/ClubRepositoryTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using RunTogetherWebApp.Data;
 using RunTogetherWebApp.Data.Enum;
+using RunTogetherWebApp.Interfaces;
 using RunTogetherWebApp.Models;
 using RunTogetherWebApp.Repositories;
 using System;
@@ -21,14 +22,14 @@ namespace RunTogetherWebApp.Tests.Repository
                 .Options;
             var databaseContext = new ApplicationDbContext(options);
             databaseContext.Database.EnsureCreated();
-            if (await databaseContext.Clubs.CountAsync() < 0)
+            if (await databaseContext.Clubs.CountAsync() == 0)
             {
                 for (int i = 0; i < 10; i++)
                 {
                     databaseContext.Clubs.Add(
                       new Club()
                       {
-                          Title = "Running Club 1",
+                          Title = $"Running Club {i + 1}",
                           Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
                           Description = "This is the description of the first cinema",
                           ClubCategory = ClubCategory.City,
@@ -39,8 +40,8 @@ namespace RunTogetherWebApp.Tests.Repository
                               State = "NC"
                           }
                       });
-                    await databaseContext.SaveChangesAsync();
                 }
+                await databaseContext.SaveChangesAsync();
             }
             return databaseContext;
         }
@@ -87,6 +88,70 @@ namespace RunTogetherWebApp.Tests.Repository
             //Assert
             result.Should().NotBeNull();
             result.Should().BeOfType<Task<Club>>();
+        }
+
+        [Fact]
+        public async Task Delete_DeletesClubFromDatabase()
+        {
+            // Arrange
+            var club = new Club()
+            {
+                Title = "Running Club 111",
+                Image = "https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/running.jpg?quality=82&strip=1&resize=640%2C360",
+                Description = "This is the description of the first cinema",
+                ClubCategory = ClubCategory.City,
+                Address = new Address()
+                {
+                    Street = "123 Main St",
+                    City = "Charlotte",
+                    State = "NC"
+                }
+            };
+
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            dbContext.Clubs.Add(club);
+            await dbContext.SaveChangesAsync();
+
+            // Act
+            var result = clubRepository.Delete(club);
+            var clubs = await dbContext.Clubs.ToListAsync();
+
+            // Assert
+            result.Should().BeTrue();
+            clubs.Should().NotContain(c => c.Title == "Running Club 111");
+        }
+
+        [Fact]
+        public async void GetAll_ReturnsAllClubs()
+        {
+            // Arrange
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+            // Act
+            var result = await clubRepository.GetAll();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(10);
+        }
+
+        [Fact]
+        public async Task GetClubByCity_ReturnsClubsInCity()
+        {
+            // Arrange
+            var dbContext = await GetDbContext();
+            var clubRepository = new ClubRepository(dbContext);
+
+
+            // Act
+            var result = await clubRepository.GetClubByCity("Charlotte");
+
+            // Assert
+            result.Should().NotBeNullOrEmpty();
+            result.First().Title.Should().Be("Running Club 1");
         }
     }
 }
