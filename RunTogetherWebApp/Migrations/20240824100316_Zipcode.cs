85258bc899ea920d2a8b6bc5a00c02301b5863b4
Kamilek <radek71k@onet.pl>
2024-08-24 12:05:22 +0200

    Added zip code to register button

---
+++ RunTogetherWebApp/Migrations/20240824100316_Zipcode.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunTogetherWebApp.Migrations
+{
+    public partial class Zipcode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ZipCode",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Addresses");
+        }
+    }
+}
